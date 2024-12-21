0d6b2046eb2901000bd6e6d4169ca59b3f0b090b
Abdul-Hayee11 <ahayes1183@gmail.com>
2024-12-21 03:12:53 +0200

    Add files via upload

---
+++ Task 4/Assignment 4/university_ranking/Task.py
@@ -0,0 +1,12 @@
+# Example 6
+import scrapy
+
+class UniversitySpider(scrapy.Spider):
+    name = "university"
+    start_urls = ["https://www.shanghairanking.com/rankings/arwu/2023"]
+
+    def parse(self, response):
+        for row in response.css('table tbody tr'):
+            rank = row.css('.ranking::text').get()
+            name = row.css('.univ-name::text').get()
+            yield {"Rank": rank, "University": name}
